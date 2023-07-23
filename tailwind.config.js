5ee819113083348b4ebec4d2b57efbb6c8478065
lonewolf <thechampthapa@gmail.com>
2023-07-23 17:17:32 +0530

    gpt homepage ui done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./templates/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        chatblack:{50:"#343541"}
+      }
+    },
   },
   plugins: [],
 }
