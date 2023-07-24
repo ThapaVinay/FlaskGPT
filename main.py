518fede6296f57ca402625ca5c1b7603b943be42
lonewolf <thechampthapa@gmail.com>
2023-07-24 17:05:05 +0530

    added js and database connectivity

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from flask import Flask, render_template, request, jsonify
+from flask_pymongo import PyMongo
+
 
 app = Flask(__name__)
+
+
+# connection to database
+mongo = PyMongo(app)
+app.config["MONGO_URI"] = "mongodb+srv://LoneWolf:priyamygf12@cluster0.luv9pon.mongodb.net/FlaskGPT?retryWrites=true&w=majority"
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/api", methods=["GET", "POST"])
 def qa():
-    # if request.method == "POST":
-    data = {"result" : "how are you"}
-    
-    return jsonify(data)
+    if request.method == "POST":
+        print(request.json)
+        question = request.json.get("question")
+        data = {"result":question}
 
+    return jsonify(data)
 
 app.run(debug = True)
---
+++ static/js/script.js
@@ -0,0 +1,26 @@
+
+async function postData(url="", data={})
+{
+    const response = await fetch(url,{
+        method:"POST",
+        headers:{
+            "Content-Type" : "application/json",
+        },
+        body: JSON.stringify(data),
+    });
+    return response.json();
+}
+
+sendButton.addEventListener("click", async ()=>{
+    question = document.getElementById("question").value;
+    document.getElementById("question").value = "";
+    document.querySelector(".right2").style.display = "flex";
+    document.querySelector(".right1").style.display = "none";
+
+    ques.innerHTML = question;
+
+    // Get the answer and populate
+    let result = await postData("/api", {"question" : question});
+    ans.innerHTML = result.result;
+
+})
